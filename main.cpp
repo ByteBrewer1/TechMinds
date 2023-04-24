bac3dc4bfc239a0621273226ba95d68071b3f3b2
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:29:26 +0530

    FL BRACKET_04

--- main.cpp
+++ main.cpp
@@ -1395,7 +1395,7 @@ void asciiart_bold(){
         if (input[i] == '[')
             cout<<" ██  ";
         if (input[i]== ']')
-            cout<<"  | | ";
+            cout<<"  ██ ";
         if (input[i] == '|')
             cout<<"| | ";
         if (input[i] == '\\')
