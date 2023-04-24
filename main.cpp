441e5ad48fdd29361b0620e4ac500759ace8f656
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:20:16 +0530

    FL L

--- main.cpp
+++ main.cpp
@@ -1315,7 +1315,7 @@ void asciiart_bold(){
         if (input[i] == 'K' || input[i] == 'k')
             cout<<" ██  ██  ";
         if (input[i] == 'L' || input[i] == 'l')
-            cout<<"| |     ";
+            cout<<" ██      ";
         if (input[i] == 'M' || input[i] == 'm')
             cout<<"| |\\/| | ";
         if (input[i] == 'N' || input[i] == 'n')
