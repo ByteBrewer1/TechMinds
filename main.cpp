4d17e089bfa662ffe601b101537e84a7b699f1f1
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 09:47:39 +0530

    SL STAR

--- main.cpp
+++ main.cpp
@@ -1129,7 +1129,7 @@ void asciiart_bold(){
         if (input[i] == '&')
             cout<<"    ██    ";
         if (input[i] == '*')
-            cout<<" /\\| |/\\  ";
+            cout<<" ▄ ██ ▄";
         if (input[i] == '(')
             cout<<" / / ";
         if (input[i] == ')')
