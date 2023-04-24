9be5976b07c24c2a2cd650896dd28149b3e3084b
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:22:48 +0530

    FL Z

--- main.cpp
+++ main.cpp
@@ -1343,7 +1343,7 @@ void asciiart_bold(){
         if (input[i] == 'Y' || input[i] == 'y')
             cout<<"    ██    ";
         if (input[i] == 'Z' || input[i]== 'z')
-            cout<<"  / /   ";
+            cout<<"  ███    ";
         if (input[i] == ' ')
             cout<<"  ";
         if (input[i] == '`')
