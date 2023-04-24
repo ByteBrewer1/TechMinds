7df61b1a197e15f5f0ea7df40800668bf5b8fc20
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 09:39:23 +0530

    SL V

--- main.cpp
+++ main.cpp
@@ -1079,7 +1079,7 @@ void asciiart_bold(){
         if (input[i] == 'T' || input[i] == 't')
             cout<<"    ██    ";
         if (input[i] == 'U' || input[i] == 'u')
-            cout<<"| | | | ";
+            cout<<" ██    ██ ";
         if (input[i] == 'V' || input[i] == 'v')
             cout<<"| | | | ";
         if (input[i] == 'W' || input[i] == 'w')
