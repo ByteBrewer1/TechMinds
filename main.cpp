60515f4edcce302d0be439efa22dcdc55b75c7a6
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:27:39 +0530

    FL AND

--- main.cpp
+++ main.cpp
@@ -1381,7 +1381,7 @@ void asciiart_bold(){
         if (input[i] == '^')
             cout<<"       ";
         if (input[i] == '&')
-            cout<<" / _ \\/\\ ";
+            cout<<" ██  ██   ";
         if (input[i] == '*')
             cout<<"|_     _| ";
         if (input[i] == '(')
