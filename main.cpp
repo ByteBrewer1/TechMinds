293dd52fbdc60dd0d58acfbce0037498bb39175a
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 09:47:58 +0530

    SL BRACKET_01

--- main.cpp
+++ main.cpp
@@ -1131,7 +1131,7 @@ void asciiart_bold(){
         if (input[i] == '*')
             cout<<" ▄ ██ ▄";
         if (input[i] == '(')
-            cout<<" / / ";
+            cout<<" ██  ";
         if (input[i] == ')')
             cout<<"\\ \\  ";
         if (input[i] == '-')
