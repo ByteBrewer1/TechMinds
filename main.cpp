da905afef7cb7aec3561d81bba4ffa068626312a
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:39:14 +0530

    5 Q

--- main.cpp
+++ main.cpp
@@ -1452,7 +1452,7 @@ void asciiart_bold(){
         if (input[i] == 'P' || input[i] == 'p')
             cout<<" ██      ";
         if (input[i] == 'Q' || input[i] == 'q')
-            cout<<"\\ \\/' / ";
+            cout<<"  ██████  ";
         if (input[i] == 'R' || input[i] == 'r')
             cout<<"| |\\ \\  ";
         if (input[i] == 'S' || input[i] == 's')
