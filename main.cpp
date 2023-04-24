9dd4f3432b0eedf5ddfa5b2c106ce2e95d6cf63d
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:31:41 +0530

    FL QM

--- main.cpp
+++ main.cpp
@@ -1411,7 +1411,7 @@ void asciiart_bold(){
         if (input[i] == '/')
             cout<<"  ██    ";
         if (input[i] == '?')
-            cout<<"  / /  ";
+            cout<<"   ▀▀    ";
     }
     cout << endl;
 }
