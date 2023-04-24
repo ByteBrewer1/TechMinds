88a77ec9543702480113ca9174707e686d37c9bd
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 09:51:16 +0530

    SL COLON

--- main.cpp
+++ main.cpp
@@ -1147,7 +1147,7 @@ void asciiart_bold(){
         if (input[i] == ';')
             cout<<" ██ ";
         if (input[i] == ':')
-            cout<<" _  ";
+            cout<<" ██ ";
         if (input[i] == '\'')
             cout<<"( ) ";
         if (input[i] == '"')
