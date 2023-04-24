f15dfbd8147346df7c7b245065bdf785fbab72ec
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 09:43:26 +0530

    SL 6

--- main.cpp
+++ main.cpp
@@ -1103,7 +1103,7 @@ void asciiart_bold(){
         if (input[i] == '5')
             cout<<" ██      ";
         if (input[i] == '6')
-            cout<<" / ___| ";
+            cout<<" ██       ";
         if (input[i] == '7')
             cout<<"|___  / ";
         if(input[i] == '.')
