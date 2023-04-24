e7b8de00741a6b20e55837b346861dfdede3990e
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:53:09 +0530

    5 COMA

--- main.cpp
+++ main.cpp
@@ -1532,7 +1532,7 @@ void asciiart_bold(){
         if (input[i] == '<')
             cout<<"   ██ ";
         if (input[i] == ',')
-            cout<<"( ) ";
+            cout<<" ▄█ ";
         if (input[i] == '>')
             cout<<" / /  ";
         if (input[i] == '/')
