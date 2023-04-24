f59c8b9d6f0e3d223cec8f2cff1d215d6824c1d8
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:50:18 +0530

    5 BRACKET_01

--- main.cpp
+++ main.cpp
@@ -1512,7 +1512,7 @@ void asciiart_bold(){
         if (input[i] == '*')
             cout<<"       ";
         if (input[i] == '(')
-            cout<<"| |  ";
+            cout<<"  ██ ";
         if (input[i] == ')')
             cout<<" | | ";
         if (input[i] == '-')
