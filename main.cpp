c41fc339bc5b8e22c3e61119ea6894a6d7d328ee
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:31:25 +0530

    FL DIVIDE

--- main.cpp
+++ main.cpp
@@ -1409,7 +1409,7 @@ void asciiart_bold(){
         if (input[i] == '>')
             cout<<"  ██  ";
         if (input[i] == '/')
-            cout<<"  / /   ";
+            cout<<"  ██    ";
         if (input[i] == '?')
             cout<<"  / /  ";
     }
