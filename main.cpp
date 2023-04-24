47546ea6a37eafc59c9041acb0398981bff95b08
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:21:50 +0530

    FL U

--- main.cpp
+++ main.cpp
@@ -1333,7 +1333,7 @@ void asciiart_bold(){
         if (input[i] == 'T' || input[i] == 't')
             cout<<"    ██    ";
         if (input[i] == 'U' || input[i] == 'u')
-            cout<<"| | | | ";
+            cout<<" ██    ██ ";
         if (input[i] == 'V' || input[i] == 'v')
             cout<<"| | | | ";
         if (input[i] == 'W' || input[i] == 'w')
