1dd70f9aead2ebe82e7160d635ed5a9fd01aafd3
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:30:16 +0530

    FL COLON

--- main.cpp
+++ main.cpp
@@ -1401,7 +1401,7 @@ void asciiart_bold(){
         if (input[i] == ';')
             cout<<" ▄█ ";
         if (input[i] == ':')
-            cout<<"    ";
+            cout<<" ██ ";
         if (input[i] == '\'')
             cout<<"    ";
         if (input[i] == '"')
