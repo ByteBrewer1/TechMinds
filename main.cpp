1d5fd910b49c3ea4d243f0e3864016880621f395
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:27:09 +0530

    FL PERC

--- main.cpp
+++ main.cpp
@@ -1377,7 +1377,7 @@ void asciiart_bold(){
         if (input[i] == '$')
             cout<<"      ██ ";
         if (input[i] == '%')
-            cout<<"  / /   ";
+            cout<<"  ██    ";
         if (input[i] == '^')
             cout<<"     ";
         if (input[i] == '&')
