fb7536f12dfa893117a36b608192fb848d5eb9bf
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:08:51 +0530

    TL BRACKET_02

--- main.cpp
+++ main.cpp
@@ -1260,7 +1260,7 @@ void asciiart_bold(){
         if (input[i] == '(')
             cout<<" ██  ";
         if (input[i] == ')')
-            cout<<" | | ";
+            cout<<"  ██ ";
         if (input[i] == '-')
             cout<<" ______  ";
         if (input[i] == '_')
