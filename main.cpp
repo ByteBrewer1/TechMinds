79d742539450fc612cfcde9903058ea44b6d690f
iusenotepadonnly <22cs042@charusat.edu.in>
2023-04-24 10:49:09 +0530

    5 DOLLAR

--- main.cpp
+++ main.cpp
@@ -1502,7 +1502,7 @@ void asciiart_bold(){
         if (input[i] == '#')
             cout<<"  ██  ██  ";
         if (input[i] == '$')
-            cout<<"(   / ";
+            cout<<" ███████ ";
         if (input[i] == '%')
             cout<<" / / _  ";
         if (input[i] == '^')
