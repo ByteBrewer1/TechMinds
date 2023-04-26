07956377365b66895d5a321d420da28a02f0115e
Rahul <22cs042@charusat.edu.in>
2023-04-26 09:30:56 +0530

    FL R

--- main.cpp
+++ main.cpp
@@ -945,7 +945,7 @@ void asciiart_3D(){
         if (input[i] == 'Q' || input[i] == 'q')
             cout<<"  /$$$$$$ ";
         if (input[i] == 'R' || input[i] == 'r')
-            cout<<" ██████  ";
+            cout<<" /$$$$$$$ ";
         if (input[i] == 'S' || input[i] == 's')
             cout<<" ███████ ";
         if (input[i] == 'T' || input[i] == 't')
